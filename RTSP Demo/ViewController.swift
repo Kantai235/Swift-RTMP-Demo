16738837b9e3880e53cdd3b10e38d4527b32fa9e
乾太 <huevo235@gmail.com>
2016-11-04 18:32:09 +0800

    Initial Commit

---
+++ RTSP Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RTSP Demo
+//
+//  Created by 乾太 on 2016/11/4.
+//  Copyright © 2016年 乾太. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
